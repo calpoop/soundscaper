23cceaf79a43358fe3d0325606b1397400ad5029
Calvin Tian Li <ctl2124@columbia.edu>
2021-07-03 22:55:00 -0400

    improve performance of wav_plot.py

--- PoC-wav-plot/wav_plot.py
+++ PoC-wav-plot/wav_plot.py
@@ -1,7 +1,7 @@
+import sys
+import wave
 import numpy as np
 import matplotlib.pyplot as plt
-import wave
-import sys
 
 if len(sys.argv) != 2:
     sys.exit("usage: python3 wav_plot.py <wav_file>")
@@ -12,9 +12,9 @@ with wave.open(filename,"r") as wav_file:
     signal = wav_file.readframes(-1)
     signal = np.fromstring(signal,np.int16)
 
-    channels = [[] for channel in range(wav_file.getnchannels())]
-    for index, datum in enumerate(signal):
-        channels[index%len(channels)].append(datum)
+    num_channels = wav_file.getnchannels()
+
+    channels = [signal[channel::num_channels] for channel in range(num_channels)]
 
     fs = wav_file.getframerate()
     Time = np.linspace(0, int(len(signal)/len(channels)/fs), num=int(len(signal)/len(channels)
@@ -25,5 +25,5 @@ with wave.open(filename,"r") as wav_file:
     plt.title("Signal Wave of \"%s\"" % filename)
     for channel in channels:
         plt.plot(Time,channel)
-    #plt.show()
+
     plt.savefig("poop.png")
